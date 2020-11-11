06dfe72f57ea4d70a40d2e1731b64f4723fb8c77
Eugenito <72038188+Eugenito@users.noreply.github.com>
2020-11-11 11:27:48 +0300

    Add files via upload

---
+++ 444.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int a[12] = { 1,3,5,2,10,11,12,13,4,5,0,1 };
+	int kl = 0;
+	int n = 1;
+	for (int i = 0;i < 12;i++)
+	{
+		
+		if (a[i] < a[i + 1])
+		{
+			n++;
+		}
+		else if(n!=1)
+		{
+			n = 1;
+			kl += 1;
+		     }
+
+
+	}
+	printf("%d", kl);
+	return 0;
+}
